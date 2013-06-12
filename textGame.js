6e0ec26617f35ee37250d647e685ca9697362213
AlisaEx <eatliightning@gmail.com>
2013-06-12 16:37:26 -0400

    textbox in html and started js aspect

---
+++ textGame.js
@@ -0,0 +1,5 @@
+var intro = "Hello there!";
+var roomNorth = "You are facing north.";
+var roomSouth = "You are facing south.";
+var roomEast = "You are facing east.";
+var roomWest = "You are facing west.";
