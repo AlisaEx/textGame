3c845f338ddaa1f715dc1604cb487b09b4242db0
AlisaEx <eatliightning@gmail.com>
2013-06-14 16:15:34 -0400

    added css and jquery

--- textGame.js
+++ textGame.js
@@ -1,5 +1,9 @@
-var intro = "Hello there!";
+var intro = "Hello there! You are in a room.";
 var roomNorth = "You are facing north.";
 var roomSouth = "You are facing south.";
 var roomEast = "You are facing east.";
 var roomWest = "You are facing west.";
+
+$(document).ready(function(){
+    $('#output').text(intro);
+})
