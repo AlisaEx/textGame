3697d6e9d109d654efd748c20e7e9149437a1187
AlisaEx <eatliightning@gmail.com>
2013-06-14 16:36:53 -0400

    added keydown event

--- textGame.js
+++ textGame.js
@@ -3,7 +3,13 @@ var roomNorth = "You are facing north.";
 var roomSouth = "You are facing south.";
 var roomEast = "You are facing east.";
 var roomWest = "You are facing west.";
+var continueOn = jQuery.Event( "keydown", { keyCode: 13 } );
 
 $(document).ready(function(){
     $('#output').text(intro);
+})
+$('input').on(continueOn,function(){
+	if (input === "n"){
+		$('#output').text(roomNorth);
+	}
 })
